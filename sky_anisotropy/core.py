1e83080c7a55a35d6d7f78b50e4548aeccb7d061
viniwiedemann <41759041+viniwiedemann@users.noreply.github.com>
2018-07-27 11:26:49 -0500

    codestyle
    fixing some issues with the codestyle and adding disc theta band to the off region fct dictionary

--- sky_anisotropy/core.py
+++ sky_anisotropy/core.py
@@ -244,6 +244,7 @@ def counts_chi_squared(counts_on, counts_off):
 off_region_funcs = {'allsky': allsky_off_region,
                     'theta_band': theta_band_off_region,
                     'opposite': opposite_off_region,
+                    'disc_theta_band': disc_theta_band_off_region
                     }
 
 
@@ -254,22 +255,20 @@ def off_region_func(name):
         raise ValueError('Invalid off_region entered ({}). Must be either '
                          '"allsky", "theta_band", or "opposite".'.format(name))
 
-def disc_theta_band_off_region(pix,pix_center,size=np.radians(10),nside=64):
+        
+def disc_theta_band_off_region(pix, pix_center, size=np.radians(10), nside=64):
     
-    theta_center, phi_center = hp.pix2ang(nside,pix_center)
+    theta_center, phi_center = hp.pix2ang(nside, pix_center)
     num_disc = np.pi*np.sin(theta_center)//size+1
     num_disc=int(num_disc)
-    phi_disc_cent = np.linspace(phi_center,phi_center+2*np.pi,num_disc)
-    theta_disc_cent = np.full(num_disc,theta_center)
+    phi_disc_cent = np.linspace(phi_center, phi_center+2*np.pi, num_disc)
+    theta_disc_cent = np.full(num_disc, theta_center)
     
-    disc_vec = hp.ang2vec(theta_disc_cent,phi_disc_cent)
+    disc_vec = hp.ang2vec(theta_disc_cent, phi_disc_cent)
     disc_list = []
-    for i in range(1,len(disc_vec)-1):
-        discs = hp.query_disc(nside,disc_vec[i,:],size)
+    for i in range(1, len(disc_vec)-1):
+        discs = hp.query_disc(nside, disc_vec[i,:], size)
         disc_list.append(discs)
     disc_arr = np.concatenate(disc_list)
     in_off_region = np.isin(pix,disc_arr)
-    
-    return in_off_regio
-
-        
+    return in_off_region
