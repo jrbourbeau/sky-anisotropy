6ec067eeb607c974b39ff881fef77ed2d9a02224
viniwiedemann <41759041+viniwiedemann@users.noreply.github.com>
2018-07-27 14:22:37 -0500

    changed definition of disc_theta_band

--- sky_anisotropy/core.py
+++ sky_anisotropy/core.py
@@ -218,6 +218,23 @@ def opposite_off_region(pix, pix_center, on_region_mask, nside=64):
     return off_region_mask
 
 
+def disc_theta_band_off_region(pix, pix_center, size=np.radians(10), nside=64):
+    theta_center, phi_center = hp.pix2ang(nside, pix_center)
+    num_disc = np.pi * np.sin(theta_center) // size + 1
+    num_disc=int(num_disc)
+    phi_disc_cent = np.linspace(phi_center, phi_center + 2 * np.pi, num_disc)
+    theta_disc_cent = np.full(num_disc, theta_center)  
+    disc_vec = hp.ang2vec(theta_disc_cent, phi_disc_cent)
+    disc_list = []
+    for i in range(1, len(disc_vec) - 1):
+        discs = hp.query_disc(nside, disc_vec[i,:], size)
+        disc_list.append(discs)
+    disc_arr = np.concatenate(disc_list)
+    in_off_region = np.isin(pix,disc_arr)
+
+    return in_off_region
+
+
 def counts_chi_squared(counts_on, counts_off):
     """ Calculates reduced chi-squared between two energy histograms
 
@@ -254,20 +271,3 @@ def off_region_func(name):
     except KeyError:
         raise ValueError('Invalid off_region entered ({}). Must be either '
                          '"allsky", "theta_band", or "opposite".'.format(name))
-
-        
-def disc_theta_band_off_region(pix, pix_center, size=np.radians(10), nside=64):
-    theta_center, phi_center = hp.pix2ang(nside, pix_center)
-    num_disc = np.pi * np.sin(theta_center) // size + 1
-    num_disc=int(num_disc)
-    phi_disc_cent = np.linspace(phi_center, phi_center + 2 * np.pi, num_disc)
-    theta_disc_cent = np.full(num_disc, theta_center)  
-    disc_vec = hp.ang2vec(theta_disc_cent, phi_disc_cent)
-    disc_list = []
-    for i in range(1, len(disc_vec) - 1):
-        discs = hp.query_disc(nside, disc_vec[i,:], size)
-        disc_list.append(discs)
-    disc_arr = np.concatenate(disc_list)
-    in_off_region = np.isin(pix,disc_arr)
-
-    return in_off_region
